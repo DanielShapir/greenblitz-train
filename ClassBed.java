9466679389ee878005a2d4fd32cece230441c6e0
daniel <danielshapir1@gmail.com>
2025-08-03 16:21:10 +0300

    checkpoint to the class question

---
+++ ClassBed.java
@@ -0,0 +1,16 @@
+public Bed{
+ private double height ;
+ private int pillowNum;
+ private String sheetColor;
+
+ public Bed(double height , int pillowNum , String sheetColor  ) {
+  this.height = height;
+  this.pillowNum = pillowNum;
+  this.sheetColor = sheetColor;
+ }
+
+public double  get( )
+
+
+
+}
